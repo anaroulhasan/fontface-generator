e6479316839c3af75f0aa99fb7a0e3f185a789c1
Anaroul Hasan <mdanaroulsarder@gmail.com>
2025-07-04 20:40:27 +0600

    Update a.js

--- a.js
+++ a.js
@@ -1,4 +1,6 @@
- document.body.style.margin = "0";
+function hackedByAman() {
+  document.body.style.margin = "0";
   document.body.style.height = "100vh";
   document.body.style.background = "black";
   document.body.innerHTML = '<div style="color: red; font-size: 48px; display: flex; justify-content: center; align-items: center; height: 100%; font-family: sans-serif;">Hacked by Aman</div>';
+}
